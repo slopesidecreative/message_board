440a59293af4f2b4f580b8c42a39aa8305efe95b
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-12-07 18:47:29 -0800

    modelrific

--- server.js
+++ server.js
@@ -13,9 +13,27 @@ app.set('views', __dirname + '/views');
 // set EJS as the templating engine
 app.set('view engine','ejs');
 
+
+
+// var mongoose = require('mongoose');
+// mongoose.connect(config['database_url']);
+
+
+
+var mongoose = require('mongoose');
+
+var db = 'mongodb://localhost/quotes';
+mongoose.connect(db,function(){
+   console.log('mongoose connected');
+});
+
+var models = require('./static/js/db')(mongoose);
+
 // MODELS ---------------------------------------
 // our connection to the User model via Mongoose
-var Item = require('./static/js/db.js');
+// var Item = require('./static/js/db.js');
+// var Item = new models.Item({name:"erik",quote:"nothing"});
+// console.log('Item: ',Item);
 
 // ROUTES --------------------------------------
 
@@ -23,7 +41,7 @@ var Item = require('./static/js/db.js');
 // Root - show all
 app.get('/', function (req, res){
    console.log('Show all items.');
-   Item.find({}, function(err, data) {
+    models.Item.find({}, function(err, data) {
       if(err){
          console.log('error: ',err);
          res.render('index', {title: 'you have errors!', errors: err})
@@ -39,7 +57,7 @@ app.get('/', function (req, res){
 */
 app.post('/', function (req, res){
    console.log('Create item: action. Quote: ',req.body.quote);
-   var item = new Item({
+   var item = new models.Item({
       name: req.body.name,
       quote: req.body.quote
    });
--- static/js/db.js
+++ static/js/db.js
@@ -1,49 +1,48 @@
-var mongoose = require('mongoose');
-var validate = require('mongoose-validator');
-
-var db = 'mongodb://localhost/quotes';
-
-mongoose.connect(db,function(){
-   console.log('mongoose connected');
-});
 
 
 
-/* ********** VALIDATIONS ********** */
-var nameValidator = [
-  validate({
-    validator: 'isLength',
-    arguments: [4, 50],
-    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
-  }),
-  validate({
-    validator: 'isAlphanumeric',
-    passIfEmpty: true,
-    message: 'Name should contain alpha-numeric characters only'
-  })
-];
-var postValidator = [
-  validate({
-    validator: 'isLength',
-    arguments: [4, 500],
-    message: 'Post should be between {ARGS[0]} and {ARGS[1]} characters'
-  })
-];
-/* ********** /END VALIDATIONS ********** */
 
-var ItemSchema = new mongoose.Schema({
-  name: {
-     type: String,
-     required: true,
-     validate: nameValidator
-  },
-  quote: {
-     type: String,
-     required: true,
-     validate: postValidator
-  }
-}, { timestamps:true });
+module.exports = function(mongoose) {
 
+   var validate = require('mongoose-validator');
+   /* ********** VALIDATIONS ********** */
+   var nameValidator = [
+     validate({
+       validator: 'isLength',
+       arguments: [4, 50],
+       message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+     }),
+     validate({
+       validator: 'isAlphanumeric',
+       passIfEmpty: true,
+       message: 'Name should contain alpha-numeric characters only'
+     })
+   ];
+   var postValidator = [
+     validate({
+       validator: 'isLength',
+       arguments: [4, 500],
+       message: 'Post should be between {ARGS[0]} and {ARGS[1]} characters'
+     })
+   ];
+   /* ********** /END VALIDATIONS ********** */
 
+   var ItemSchema = new mongoose.Schema({
+     name: {
+        type: String,
+        required: true,
+        validate: nameValidator
+     },
+     quote: {
+        type: String,
+        required: true,
+        validate: postValidator
+     }
+   }, { timestamps:true });
+    // declare seat covers here too
+    var models = {
+      Item: mongoose.model('item', ItemSchema)
+    };
+    return models;
 
-module.exports = mongoose.model('item',ItemSchema);
+}
