821c0d7a13684544c40ff19c51b355dfa52c72f4
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-12-08 16:07:54 -0800

    change char validation

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(db, function(){
    var nameValidator = [
      validate({
        validator: 'isLength',
-       arguments: [4, 50],
+       arguments: [2, 50],
        message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
      }),
      validate({
